3a8a3653ee3d4d0b470d6e760a26d5a5342b047f
Thiago Felipe <thiagof.g3@gmail.com>
2024-06-04 15:52:15 -0300

    feat: criando a camada de configs utilizando e impostando middleware routes e envs

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+export default {
+    port: process.env.PORT || 5050,
+}
---
+++ src/main/config/middlewares.ts
@@ -0,0 +1,8 @@
+import { Express } from 'express'
+import { bodyParse, contentType, cors } from '../middlewares'
+
+export default (app: Express): void => {
+    app.use(bodyParse)
+    app.use(cors)
+    app.use(contentType)
+}
